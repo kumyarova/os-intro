ca2dbf2513093e4c8103afc95da72aa4c1474084
kumyarova <1132229051@pfur.ru>
2023-04-07 22:46:57 +0300

    lab 9

---
+++ labs/lab09/hello.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+HELLO=Hello
+function hello
+{
+local HELLO=World
+local HELLO=World
+local HELLO=World
+local HELLO=World
+local HELLO=World
+local HELLO=World
+local HELLO=World
+local HELLO=World
+echo $HELLO
+}
+echo $HELLO
+hello
+local HELLO=World
+local HELLO=World
+local HELLO=World
+local HELLO=World
+echo $HELLO
+}
+echo $HELLO
+hello
